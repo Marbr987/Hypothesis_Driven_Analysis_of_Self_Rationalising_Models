bc2dc6a20023b616db19e518ba0fb6ab2bc249e0
Marc Braun <marc.y.braun@live.de>
2023-02-01 15:00:45 +0100

    feature: Add code for preparing GPT-3 prompts

--- 01_GPT3_Explanations/helper_funcs.py
+++ 01_GPT3_Explanations/helper_funcs.py
@@ -3,17 +3,26 @@ import random
 import numpy as np
 import pandas as pd
 
-def single_row_to_string(row, include_label):
+def single_row_to_string(row, style):
     s1 = row.Sentence1
     s2 = row.Sentence2
     expl = row.Explanation_1
     label= row.gold_label
-    if include_label:
-        return 'Statement: ' + s1 + '\nStatement: ' + s2 + '\nLabel: ' + label + '\nExplanation: ' + expl
+    label_map = {"entailment": "Yes",
+                 "neutral": "Maybe",
+                 "contradiction": "No"}
+    if style=="GPT-3":
+        return ('Premise: ' + s1 + \
+                '\nHypothesis: ' + s2 + \
+                '\nLabel: ' + label + \
+                '\nExplanation: ' + expl + '\n###\n')
     else:
-        return 'Statement: ' + s1 + '\nStatement: ' + s2 + '\nExplanation: ' + expl
+        return (s1 +\
+           '\nQuestion: Is ' + s2.lower().replace(".", "") +\
+           '?\nAnswer: ' + label_map[label] +\
+           '\nReason: ' + expl + '\n###\n')
 
-def prepare_examples(data, size_per_class=4, include_label=False, example_indices=False):
+def prepare_examples(data, size_per_class=4, style="GPT-3", example_indices=False):
     if not example_indices:
         example_indices = list()
         for cat in ['neutral', 'contradiction', 'entailment']:
@@ -22,13 +31,19 @@ def prepare_examples(data, size_per_class=4, include_label=False, example_indice
     data = data.loc[example_indices]
     res = list()
     for row in data.itertuples():
-        res += [single_row_to_string(row, include_label=include_label)]
-    return '\n\n'.join(res)
+        res += [single_row_to_string(row, style=style)]
+    if style=="GPT-3":
+        return ''.join(res)
+    else:
+        return 'Answer the Question and provide a reason why the answer is correct.\n\n' + ''.join(res)
 
-def create_query(row, include_label=False):
+def create_query(row, style="GPT-3"):
     s1 = row.Sentence1
     s2 = row.Sentence2
-    if include_label:
-        return 'Statement: ' + s1 + '\nStatement: ' + s2 + '\nLabel: '
-    else:
-        return 'Statement: ' + s1 + '\nStatement: ' + s2 + '\nExplanation: '
+    if style=="GPT-3":
+        return ('Premise: ' + s1 + \
+                '\nHypothesis: ' + s2 + \
+                '\nLabel: ')
+    return (s1 +\
+           '\nQuestion: Is ' + s2.lower().replace(".", "") +\
+           '?\nAnswer: ')
